ff683edd52306990d87014940d91d38ca9be55fc
Jason Sheehy <jason@hubdoc.com>
2020-04-27 19:00:56 -0400

    Added missing client app files

---
+++ dotnet/Controllers/SignupInfoController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dotnet.Models;
+
+namespace dotnet.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class SignupInfoController : ControllerBase
+  {
+    private readonly ILogger<SignupInfoController> _logger;
+    private readonly List<SignupInfo> _signups;
+
+    public SignupInfoController(ILogger<SignupInfoController> logger)
+    {
+      _logger = logger;
+      _signups = new List<SignupInfo>()
+      {
+        new SignupInfo()
+        {
+          Id = 1, FirstName = "Joe", LastName = "Smith"
+        },
+        new SignupInfo()
+        {
+          Id = 2, FirstName = "Ron", LastName = "Zil"
+        }
+      };
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<SignupInfo>>> Get()
+    {
+      _logger.LogInformation("Entering SignupInfoController.Get", null);
+
+      _logger.LogInformation("Exiting SignupInfoController.Get", null);
+
+      return _signups;
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<SignupInfo>> Get(int id)
+    {
+      var info = _signups.Where(signup => signup.Id == id).FirstOrDefault();
+
+      if (info != null)
+      {
+        return info;
+      }
+      else
+      {
+        return NotFound();
+      }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<SignupInfo>> Post(SignupInfo newSignup)
+    {
+      _signups.Add(newSignup);
+
+      _logger.LogInformation("Added new signup with Id {id}", newSignup.Id);
+
+      return newSignup;
+    }
+  }
+}
---
+++ dotnet/README.md
@@ -0,0 +1,14 @@
+# Installing dotnet core
+
+* Install the dotnet sdk through brew `brew cask install dotnet`
+
+* Initialize new sln: `dotnet new sln -o [solution name]`
+* * `cd [solution name]`
+* * Adding new project `dotnet new reactredux -o [project name]`
+* * Add project to sln `dotnet sln add [path to project .csproj file]`
+* * Add new test project `dotnet new xunit -o [<project name>.Test]`
+* * Add test project to sln `dotnet sln add [path to test project .csproj file]`
+
+* To add dependencies (equivalent of `npm install [dependency]`) run `dotnet add package [package-name]` in the root folder
+
+* To run test project `dotnet test [test project name]`
